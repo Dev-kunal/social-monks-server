f199d8ce1420c1bb88bd66375eb2a97202410139
kunal7722 <dev35.programmingtree@gmail.com>
2021-07-22 13:00:48 +0530

    remove bug while getting followers

--- controllers/user/followers.js
+++ controllers/user/followers.js
@@ -3,7 +3,10 @@ const Following = require("../../models/following.model");
 const followers = async (req, res) => {
   try {
     let { userId } = req.body;
-    const followers = await Following.find({ followingId: userId })
+    const filter = {
+      $and: [{ followingId: userId }, { followStatus: "following" }],
+    };
+    const followers = await Following.find(filter)
       .populate({
         path: "userId",
         select: "fullname username profileUrl",
